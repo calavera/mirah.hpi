45cf7a8dd7acfa86d4e88d2524bbf1f2b251f1d9
David Calavera <david.calavera@gmail.com>
2010-11-22 20:11:21 +0100

    move non abstract methods to the abstract class to clean mirah's code

--- src/main/java/org/mirah/BuilderStepDescriptor.java
+++ src/main/java/org/mirah/BuilderStepDescriptor.java
@@ -1,7 +1,15 @@
 package org.mirah;
 
 public abstract class BuilderStepDescriptor extends hudson.tasks.BuildStepDescriptor<hudson.tasks.Builder> {
-    protected BuilderStepDescriptor(Class clazz) {
+    public BuilderStepDescriptor() {
+        super();
+    }
+
+    public BuilderStepDescriptor(Class<? extends hudson.tasks.Builder> clazz) {
         super(clazz);
+    }
+
+    public boolean isApplicable(Class<? extends hudson.model.AbstractProject> project) {
+        return true;
     }
 }
